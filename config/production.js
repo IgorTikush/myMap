f0a709285df703b0087e4f7177e9e31a3ab50682
IgorTikush <igortikush@mail.ru>
2023-04-09 23:32:15 +0700

    production config

--- config/production.js
+++ config/production.js
@@ -1,6 +1,6 @@
 const config = {};
 
-config.mongo = process.env.MONGO_PATH;
+config.mongo = process.env.DB_PATH;
 config.jwtRefreshSecret = process.env.JWT_REFRESH_SECRET;
 config.jwtAccessSecret = process.env.JWT_ACCESS_SECRET;
 

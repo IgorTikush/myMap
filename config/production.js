aec6305684e120a88c04d0b8ab618bc01d882dfb
IgorTikush <igortikush@mail.ru>
2023-09-17 01:09:01 +0700

    deploy config

--- config/production.js
+++ config/production.js
@@ -4,5 +4,11 @@ config.mongo = process.env.DB_PATH;
 config.jwtRefreshSecret = process.env.JWT_REFRESH_SECRET;
 config.jwtAccessSecret = process.env.JWT_ACCESS_SECRET;
 
+config.aws = {
+  bucketName: process.env.BUCKET_NAME,
+  accessKey: process.env.AWS_ACCESS_KEY,
+  keyId: process.env.AWS_KEY_ID,
+};
+
 module.exports = config;
 
